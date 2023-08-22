7a9ec519c226715c87807ac6967a6ba6fbe3bef9
marufbellah <90157254+Maruf-Bellah@users.noreply.github.com>
2023-08-22 11:52:02 +0600

    import and export is finished

---
+++ import/external.js
@@ -0,0 +1,16 @@
+// export const pi = 3.94465; // named export
+// export const maruf = 1996; // named export
+
+
+
+
+// default ভাবে যদি পেতে চাই তাহলে এভাবে করতে পারো  ================
+export const pi = 6.5654854;
+export const maruf = 'ayesh';
+
+// default একবার করা যায় 
+
+// function কে যদি export করতে চান =================
+export default function myFunc(){
+    console.log('I am myFunc()');
+}
---
+++ import/playground.js
@@ -0,0 +1,13 @@
+// import {pi, maruf} from './external.js'; // named import / কোন কিছু নিতে গেলে এভাবে লিখবে 
+// import {pi as bellah, maruf as ayesha} from './external.js'; // named import / কোন কিছু নিতে গেলে এভাবে লিখবে  এভাবে লেখা যায় 
+
+// import * as test from './external.js'; // আর সব কিছু নিতে গেলে এভাবে লিখবে এবং as test এটাকে বলে এলিয়াজ 
+
+
+
+// default ভাবে করতে চাইলে এভাবে করবে ============
+import  external, {maruf, pi} from './external.js'
+
+console.log(maruf, pi);
+
+external()
