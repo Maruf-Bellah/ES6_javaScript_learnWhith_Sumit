b2bfa2577e83b5bac9d8e7aa200de6c5b7359e2d
marufbellah <mdmaruf5511507@gmail.com>
2023-08-29 12:32:29 +0600

    variable finished

---
+++ variable.js
@@ -0,0 +1,25 @@
+// rabbil hasan ===================================
+
+
+
+
+
+// variable ===============================
+
+/* 
+var names = "maruf bellah";
+names = "maruf" // reassing করা বলে 
+var names = "ayesha bellha"; // redecliare করা বলে 
+// console.log(name);
+
+let maruf = 'amar sonar bangla';
+maruf = 'ami tomay valobahsi'; // reassing করা যায় , redecriaring করা যায় না 
+// let maruf = 'eta kora jay na '
+// console.log(maruf);
+
+const ayesha = 'ayesha bellah';
+// ayesh = 'hobe na ';
+// const ayesha = 'hobe na '
+
+
+*/
