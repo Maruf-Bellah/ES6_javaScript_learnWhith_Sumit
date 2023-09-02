b27fb1fc3b372e41020f6556f6696a6c5910a663
marufbellah <mdmaruf5511507@gmail.com>
2023-09-02 22:16:17 +0600

    class inheritance

---
+++ class_Inheritans.js
@@ -0,0 +1,26 @@
+class parent{
+
+    hello1(){
+        console.log("Hello 1 function");
+    }
+    hello2(){
+        console.log("Hello 2 function");
+    }
+    hello3(){
+        console.log("Hello 3 function");
+    }
+}
+class child extends parent {
+
+    hello3(){
+        console.log("Hello 3 function Hello 3 function");
+        // cheler khomota babar khomota theke ektu beshi karon baba jokhon bura hoye jay tokhon chele jai bole babar ta shunte jai 
+    }
+
+}
+
+var obj = new child();
+// var obj = new parent();
+obj.hello1()
+obj.hello2()
+obj.hello3()
